e2e291e5bd8885de45de8a6ab42eb5f874d6bddc
haojunsng <snghaojun18@gmail.com>
2025-06-16 22:40:50 +0800

    [DATRUN-61] [feat] Create s3 bucket for lambda and configure OIDC for github actions

--- aws/s3.tf
+++ aws/s3.tf
@@ -10,3 +10,16 @@ resource "aws_s3_bucket_public_access_block" "privatise_bucket" {
   block_public_policy = true
   ignore_public_acls  = true
 }
+
+resource "aws_s3_bucket" "sh_datrun_lambda_bucket" {
+  bucket        = "sh-datrun-lambda"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "privatise_lambda_bucket" {
+  bucket = aws_s3_bucket.sh_datrun_lambda_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+}
